2a21bba9a4a8895395065a61c632f1638ec50a4b
95hyun <kingmandoo95@gmail.com>
2025-02-20 20:27:27 +0900

    init

---
+++ src/main/java/com/lgcns/newspacebackend/NewspaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.newspacebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspaceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewspaceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lgcns/newspacebackend/NewspaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns.newspacebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspaceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
