3f80a163749669cf5055a29a6b7f77b76e7cdc8e
minnnseokk <msjeong991029@gmail.com>
2025-02-23 01:20:14 +0900

    feat : optional<User> 타입으로 변경

--- src/main/java/com/lgcns/newspacebackend/domain/user/repository/UserRepository.java
+++ src/main/java/com/lgcns/newspacebackend/domain/user/repository/UserRepository.java
@@ -12,7 +12,7 @@ import com.lgcns.newspacebackend.domain.user.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-    User findByUsername(String username);
+	Optional<User> findByUsername(String username);
 
     Optional<User> findUserByAccessToken(String accessToken);
 
