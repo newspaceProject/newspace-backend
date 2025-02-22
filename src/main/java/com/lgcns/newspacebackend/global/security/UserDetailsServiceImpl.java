8aada0081aee96043692739e131d05f672e90b73
minnnseokk <msjeong991029@gmail.com>
2025-02-23 01:25:47 +0900

    feat : 유저 디테일 서비스 orElseThrow() 추가

--- src/main/java/com/lgcns/newspacebackend/global/security/UserDetailsServiceImpl.java
+++ src/main/java/com/lgcns/newspacebackend/global/security/UserDetailsServiceImpl.java
@@ -14,9 +14,10 @@ import com.lgcns.newspacebackend.domain.user.repository.UserRepository;
 public class UserDetailsServiceImpl implements UserDetailsService{
     @Autowired
     private UserRepository userRepository;
-
+    
+    // 유저 이름으로 레포지토리에서 찾아내는 클래스
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException { 
         User userEntity = userRepository.findByUsername(username).orElseThrow(()-> new IllegalIdentifierException("user is not exist"));
         if (userEntity == null) {
             throw new UsernameNotFoundException("등록된 사용자가 없습니다.");
