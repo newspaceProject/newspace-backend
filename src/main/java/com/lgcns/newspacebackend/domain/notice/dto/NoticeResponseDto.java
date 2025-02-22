6a20d0fb636dcecee6971f9e5570adb07d3c6c75
95hyun <kingmandoo95@gmail.com>
2025-02-22 20:33:38 +0900

    feat : 공지사항 CRUD

--- src/main/java/com/lgcns/newspacebackend/domain/notice/dto/NoticeResponseDto.java
+++ src/main/java/com/lgcns/newspacebackend/domain/notice/dto/NoticeResponseDto.java
@@ -1,4 +1,15 @@
 package com.lgcns.newspacebackend.domain.notice.dto;
 
+import com.lgcns.newspacebackend.domain.notice.entity.Notice;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class NoticeResponseDto {
+    private String content;
+
+    public NoticeResponseDto(Notice notice) {
+        this.content = notice.getContent();
+    }
 }
