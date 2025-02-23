f84cebade0a711036ab924991b97229e1b5c3c17
95hyun <kingmandoo95@gmail.com>
2025-02-24 00:29:01 +0900

    fix : 카테고리 entity, 등록, 수정에 icon 누락

--- src/main/java/com/lgcns/newspacebackend/domain/news/dto/NewsCategoryResponseDto.java
+++ src/main/java/com/lgcns/newspacebackend/domain/news/dto/NewsCategoryResponseDto.java
@@ -7,12 +7,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class NewsCategoryResponseDto {
-
     private Long id;
     private String name;
+    private String icon;
 
     public NewsCategoryResponseDto(NewsCategory category) {
         this.id = category.getId();
         this.name = category.getName();
+        this.icon = category.getIcon();
     }
 }
