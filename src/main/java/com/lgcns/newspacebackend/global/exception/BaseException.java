1fa7fad97c3cf45d6450f2505b44d552d9e192f1
95hyun <kingmandoo95@gmail.com>
2025-02-26 17:09:39 +0900

    fix : 오류 수정

--- src/main/java/com/lgcns/newspacebackend/global/exception/BaseException.java
+++ src/main/java/com/lgcns/newspacebackend/global/exception/BaseException.java
@@ -5,7 +5,6 @@ public class BaseException extends RuntimeException {
     private BaseResponseStatus status;
 
     public BaseException(BaseResponseStatus status) {
-        super(status.getMessage());
         this.status = status;
     }
 }
